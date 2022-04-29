663b5a7cd3d66cfca04b13fc0ac9c4d5f3ecf688
lyh <1945633595@qq.com>
2022-04-29 15:01:04 +0800

    MyBatis-Plus逆向生成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,9 +1,31 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 1、整合MyBatis-Plus
+ *      1）、导入依赖
+ *      <dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.2.0</version>
+ *      </dependency>
+ *      2）、配置
+ *          1、配置数据源；
+ *              1）、导入数据库的驱动。https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-versions.html
+ *              2）、在application.yml配置数据源相关信息
+ *          2、配置MyBatis-Plus；
+ *              1）、使用@MapperScan
+ *              2）、告诉MyBatis-Plus，sql映射文件位置
+ *
+ */
 
 @SpringBootApplication
+@MapperScan(value = "com.atguigu.gulimall.product.dao")
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.atguigu.gulimall.product;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+@MapperScan(value = "com.atguigu.gulimall.product.dao")
+public class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("修改");
+        brandService.updateById(brandEntity);
     }
-
 }
